100d1aea74f4883a5d18bd3870bbe2acc1aeb913
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-09 16:13:52 -0500

    Update create_class_table.sql

--- SQL Scripts/create_class_table.sql
+++ SQL Scripts/create_class_table.sql
@@ -1,5 +1,6 @@
 USE classyschedule;
 CREATE TABLE class (
+    class_id INT NOT NULL AUTO_INCREMENT,
     class_num SMALLINT NOT NULL,
     dept_id INT NOT NULL,
     class_name VARCHAR(150) NULL,
@@ -7,7 +8,7 @@ CREATE TABLE class (
     credits TINYINT NULL,
     is_lab BOOLEAN NOT NULL,
     FOREIGN KEY (dept_id)
-		REFERENCES department(dept_id)
+	REFERENCES department(dept_id)
         ON DELETE CASCADE,
-	PRIMARY KEY (class_num, dept_id, is_lab)
+	PRIMARY KEY (class_id)
 );
