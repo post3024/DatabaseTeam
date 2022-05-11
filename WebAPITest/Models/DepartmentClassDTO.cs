1bb02c330e719a07c0a6c5fbe03ce301d642ab63
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-10 19:15:32 -0500

    Fixed Department/Class Join

--- WebAPITest/Models/DepartmentClassDTO.cs
+++ WebAPITest/Models/DepartmentClassDTO.cs
@@ -2,18 +2,22 @@
 {
     public class DepartmentClassDTO
     {
+        public int class_id { get; set; }
+
         public int class_num { get; set; }
 
         public int dept_id { get; set; }
 
         public string class_name { get; set; }
 
-        public string dept_name { get; set; }
-
         public int capacity { get; set; }
 
         public int credits { get; set; }
 
         public bool is_lab { get; set; }
+
+        public int num_sections { get; set; }
+
+        public string dept_name { get; set; }
     }
 }
