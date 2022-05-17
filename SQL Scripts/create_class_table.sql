aeb0cb24868a8204a72cd24d132fab9831bcbee7
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-17 17:26:42 -0500

    Update create_class_table.sql

--- SQL Scripts/create_class_table.sql
+++ SQL Scripts/create_class_table.sql
@@ -1,13 +1,12 @@
-USE classyschedule;
-CREATE TABLE class (
-    class_id INT NOT NULL AUTO_INCREMENT,
-    class_num SMALLINT NOT NULL,
-    dept_id INT NOT NULL,
-    class_name VARCHAR(150) NULL,
-    capacity SMALLINT NULL,
-    credits TINYINT NULL,
-    is_lab BOOLEAN NOT NULL,
-    num_sections TINYINT NOT NULL,
-    PRIMARY KEY (class_id),
-    FOREIGN KEY (dept_id) REFERENCES department(dept_id) ON DELETE CASCADE
+CREATE TABLE section_time_slot(
+    section_time_slot_id INTEGER AUTO_INCREMENT,
+    on_monday BOOLEAN DEFAULT false,
+    on_tuesday BOOLEAN DEFAULT false,
+    on_wednesday BOOLEAN DEFAULT false,
+    on_thursday BOOLEAN DEFAULT false,
+    on_friday BOOLEAN DEFAULT false,
+    time_slot_id INTEGER NOT NULL,
+    PRIMARY KEY (section_time_slot_id),
+    FOREIGN KEY (time_slot_id) REFERENCES time_slot(time_slot_id) ON DELETE CASCADE,
+	UNIQUE (on_monday, on_tuesday, on_wednesday, on_thursday, on_friday, time_slot_id)
 );
