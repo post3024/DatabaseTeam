db8429083ddb9637d4a03ccaccfe1e31135d923f
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-18 09:03:37 -0500

    Update create_department_table.sql

--- SQL Scripts/create_department_table.sql
+++ SQL Scripts/create_department_table.sql
@@ -1,8 +1,10 @@
 USE classyschedule;
+--This script will create teh department table
 CREATE TABLE department (
     dept_id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(4) NOT NULL UNIQUE,
     PRIMARY KEY (dept_id)
 );
-
-INSERT INTO department (dept_name) VALUES ('CISC'), ('STAT');
+-- Insert the STAT and CISC records into the table
+INSERT INTO department (dept_name) 
+VALUES ('CISC'), ('STAT');
