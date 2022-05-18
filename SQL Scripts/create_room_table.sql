298565a05d26f51a97f3c143c9c438966f16ee54
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-18 09:08:52 -0500

    Update create_room_table.sql

--- SQL Scripts/create_room_table.sql
+++ SQL Scripts/create_room_table.sql
@@ -1,7 +1,9 @@
 USE classyschedule;
+-- This script will create the room table
 CREATE TABLE room(
     room_id INTEGER AUTO_INCREMENT,
     capacity INTEGER,
     room_num INTEGER,
+    building_name VARCHAR(4),
     PRIMARY KEY(room_id)
 );
