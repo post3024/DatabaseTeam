1636b77669e4b41ef5db20bb5b71fc01cfc46310
boro8026 <71035732+boro8026@users.noreply.github.com>
2022-05-18 09:06:31 -0500

    Update create_professor_table.sql

--- SQL Scripts/create_professor_table.sql
+++ SQL Scripts/create_professor_table.sql
@@ -1,4 +1,5 @@
 USE classyschedule;
+-- This script will create the professor table
 CREATE TABLE professor (
     professor_id INT AUTO_INCREMENT PRIMARY KEY,
     professor_first_name VARCHAR(40),
@@ -6,6 +7,7 @@ CREATE TABLE professor (
     teach_load INT,
     user_email VARCHAR(50) NOT NULL UNIQUE,
     user_password VARCHAR(256) NOT NULL,
+    -- the salt key used to encrypt the password
     salt VARCHAR(128) NOT NULL,
     user_role VARCHAR(20) NOT NULL
 );
